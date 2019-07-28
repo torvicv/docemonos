36f03f99959a826df291f51d26ee17ef609c6e8c
victor <torvicv@gmail.com>
2019-07-28 19:29:48 +0200

    tercer commit

--- src/main/java/com/victor/docemonos/controladores/BDController.java
+++ src/main/java/com/victor/docemonos/controladores/BDController.java
@@ -1,26 +1,75 @@
 package com.victor.docemonos.controladores;
 
 import java.util.Date;
+import java.util.List;
 
 import com.victor.docemonos.modelos.Employee;
+import com.victor.docemonos.servicios.EmployeeService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.log4j.Log4j2;
 
 /**
  * RestController
  */
 @RestController
+@Log4j2
 @RequestMapping("/employee")
 public class BDController {
 
-    @GetMapping("/add")
-    public Employee addEmployee() {
-        Date today = new Date();
 
-        Employee miEmpleado = new Employee(7, "Pepe", "Garcia", today);
+    @Autowired
+    private EmployeeService employeeService;
 
-        return miEmpleado;
+    @GetMapping("/showAll")
+    public ResponseEntity<List<Employee>> showEmployee() {
+       return ResponseEntity.ok(employeeService.showAll());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+
+        return ResponseEntity.ok(employeeService.guardar(employee));   
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Integer id, @RequestBody Employee employee) {
+        final Logger logger2 = LoggerFactory.getLogger(BDController.class);
+        
+        if (!employeeService.findById(id).isPresent()) {
+            logger2.error("Id " + id + " not exists.");
+
+            ResponseEntity.badRequest().build();
+        }
+        
+        return ResponseEntity.ok(employeeService.guardar(employee));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteEmployee(@PathVariable Integer id) {
+        final Logger logger2 = LoggerFactory.getLogger(BDController.class);
+        
+        if (!employeeService.findById(id).isPresent()) {
+            logger2.error("Id " + id + " not exists.");
+
+            ResponseEntity.badRequest().build();
+        }
+
+        employeeService.deleteById(id);
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/victor/docemonos/modelos/Employee.java
+++ src/main/java/com/victor/docemonos/modelos/Employee.java
@@ -2,17 +2,32 @@ package com.victor.docemonos.modelos;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * Employee
  */
+@Entity
+@Data
+@AllArgsConstructor
 public class Employee {
+
+    public Employee() {}
 
     public Employee(int i, String string, String string2, Date date) {
         this.id = i;
         this.nombre = string;
         this.apellido = string2;
         this.fecha = date;
-	}
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
     private String nombre;
     private String apellido;
